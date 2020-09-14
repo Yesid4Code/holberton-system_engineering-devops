890cf89e9a558c63f8bb9966b090c68b8cd23676
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2020-09-14 23:17:48 +0000

    code replaced

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,22 +3,23 @@
     Python script that get data for an employee using a REST API.
     Returns information about his/her TODO list progress.
 """
+
 import requests
 from sys import argv
 
-
 if __name__ == "__main__":
-    user_id = argv[1]
-    url = "https://jsonplaceholder.typicode.com/"
-    user_req = "{}users/{}".format(url, user_id)
-    user_name = requests.get(user_req, verify=False).json().get("name")
-    tasks = requests.get("{}todos?userId={}".format(url, user_id), verify=False).json()
-    total_tasks = len(tasks)
-    tasks_done = requests.get("{}todos?userId={}&&completed=true".
-                              format(url, user_id), verify=False).json()
-    total_tasks_done = len(total_tasks_done)
+    url = 'https://jsonplaceholder.typicode.com/'
 
-    print("Employee {} is done with tasks({}/{})".
-          format(user_name, total_tasks_done, total_tasks))
+    user_id = '{}users/{}'.format(url, argv[1])
+    request_user = requests.get(user_id).json()
+    name_user = request_user['name']
+    tasks_user = requests.get('{}todos?userId={}'.format(url, argv[1])).json()
+    len_tasks = len(tasks_user)
+    tasks_done = requests.get('{}todos?userId={}&&completed=true'
+                              .format(url, argv[1])).json()
+    len_tasks_done = len(tasks_done)
+
+    print('Employee {} is done with tasks({}/{}):'
+          .format(name_user, len_tasks_done, len_tasks))
     for task in tasks_done:
-        print("\t {}".format(task["title"]))
+        print('\t {}'.format(task['title']))
