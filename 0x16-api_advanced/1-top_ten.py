b3b05a5356921701bbe8e0c34e4c8fecaa0e855c
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2020-09-18 01:20:08 +0000

    define the limit inside the url

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
 """
-    Request from a API of reddit number of subscriber to a subreddit.
+    Request from a API of reddit Top 10number of subscriber to a subreddit.
 """
 import requests
 
 
 def top_ten(subreddit):
     """ Function that request the number of subscribers """
-    url = "https://api.reddit.com/r/{}/hot".format(subreddit)
-    response = requests.get(url, headers={"User-Agent": "Python3"}).json()
-    if str(response) != "<Response [200]>":
+    url = "https://api.reddit.com/r/{}/hot?limit=10".format(subreddit)
+    response = requests.get(url, headers={"User-Agent": "Python3"})
+    if str(response) != "<Response [200]>":  # response.status_code != 200
         print(None)
         return
-    # top_10 = response["data"]["children"]
-    return 0  # response["data"]["subscribers"]
+    # print(response.json())
+    response = response.json()
+    child = response["data"]["children"]
+    tile = []
+    i = 0
+    for tittle in child:
+        print(tittle["data"]["title"])
