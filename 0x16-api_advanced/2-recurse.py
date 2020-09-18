2079d92ec30ddf168dfb94027e517a3711c49350
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2020-09-18 02:24:20 +0000

    hot articles from the API of reddit

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+    Request from a API of reddit the titles of all hot articles
+    for a given subreddit.
+    Recursively.
+"""
+import requests
+
+
+def recurse(subreddit, hot_list=[], next=None):
+    """ Function that request a list of articles. """
+    if next is None:
+        return []
+
+    url = "https://api.reddit.com/r/{}/hot.json?limit=100&after={}".format(
+        subreddit, next)
+
+    response = requests.get(url, headers={"User-Agent": "Python3"})
+    if str(response) != "<Response [200]>":  # response.status_code != 200
+        return None
+    response = response.json()
+    child = response["data"]["children"]
+    next = response["data"]["after"]
+    for tittle in child:
+        hot_list.append(tittle["data"]["title"])
+    return hot_list + recurse(subreddit, [], after)
