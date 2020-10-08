ce1cf4124a5afb388efe5aaab6cf98120a8c2a30
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2020-10-08 00:12:46 -0500

    Increase the limit of user

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,6 @@
+# Increases the user limit
+exec { 'fix limit':
+  onlyif  => 'test -e /etc/security/limits.conf',
+  command => 'sed -i "s/5/4096/; s/4/1024/;" /etc/security/limits.conf',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
